d7bb1081e4d1875cdf7d9a9e87ac9347d4ef5860
noahvanes <noahvanes@me.com>
2015-05-03 13:01:00 +0200

    added a shell version
    can be used with the SpiderMonkey command-line shell as well. Load
    using ‘js spiderslip.js’

---
+++ spiderslip.js
@@ -0,0 +1,49 @@
+
+function scanBrackets(txt) {
+
+	var idx = 0;
+	var count = 0;	
+	var len = txt.length;
+	var currentChar;
+
+	while(idx < len) {
+		currentChar = txt.charAt(idx++);
+		if (currentChar === '(')
+			++count;
+		else if (currentChar === ')')
+			--count;
+	}
+
+	return count;
+}
+
+function readExpression(clb) { 
+
+	var txt = '';
+	do {
+		txt += readline();
+	} while(scanBrackets(txt));
+	clb(txt);
+}
+
+function readFile(path, clb) {
+	var str = read(path);
+	clb(path);
+}
+
+// MAIN
+
+var stdlib = {Math: Math, Uint32Array: Uint32Array, Uint8Array: Uint8Array, Float32Array: Float32Array}
+load("./slip.js");
+
+var slip = SLIP({
+	stdlib: stdlib,
+	readExpression: readExpression,
+	load: readFile,
+	print: putstr,
+	printline: print,
+	printerror: print,
+	printlog: putstr
+});
+
+slip.Slip_REPL();
