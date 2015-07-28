4663b02ebb6b97f386ac9ec954b492b8575fd61d
Noah Van Es <noahves@vub.ac.be>
2015-07-28 14:08:49 +0200

    fixed I/O for jscslip
    * readFile works fine now
    * fixed some of the ugly newlines etc…

--- jscslip.js
+++ jscslip.js
@@ -16,18 +16,51 @@ function scanBrackets(txt) {
 	return count;
 }
 
-function readExpression(clb) { 
+function readExp(clb) {
+
+	if (__KEEP__.length) {
+		print(__KEEP__);
+		__KEEP__ = ''
+	}
+	clb(readExpression());
+}
+
+function readExpression() { 
 
 	var txt = '';
 	do
 		txt += readline();
 	while(scanBrackets(txt) || !txt.trim().length);
-	clb(txt);
+	return txt;
 }
 
-function readFile(path, clb) {
-	var str = read(path);
+function readFS(path, clb) {
+	var str = readFile(path);
 	clb(str);
+}
+
+var __KEEP__ = '';
+
+function write(str) {
+	/* temporary fix to avoid printing a promt 
+	if(str === '> ') {
+		return; 
+	}
+	*/
+	var sp = str.split('\s');
+	var last = sp.length - 1;
+	for(var i = 0; i < last; ++i) {
+		__KEEP__ += sp[i];
+		print(__KEEP__);
+		__KEEP__ = '';
+	}
+	__KEEP__ = sp[last];
+}
+
+function writeLine(str) {
+	__KEEP__ += str;
+	print(__KEEP__);
+	__KEEP__ = '';
 }
 
 // MAIN
@@ -43,12 +76,12 @@ var stdlib = {
 
 var slip = SLIP({
 	stdlib: stdlib,
-	readExpression: readExpression,
-	load: readFile,
-	print: print,
-	printline: print,
-	printerror: print,
-	printlog: print
+	readExpression: readExp,
+	load: readFS,
+	print: write,
+	printline: writeLine,
+	printerror: writeLine,
+	printlog: write
 }, 26);
 
 slip.Slip_REPL();
